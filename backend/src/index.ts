22a3b72ccf71bde49088d974807df033ecc6865e
Priyanshu Gupta <priyanshu711403@gmail.com>
2024-05-13 17:46:59 +0530

    signup, signin and middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,53 @@
-import { Hono } from "hono";
+import { Hono, HonoRequest } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { decode, sign, verify } from "hono/jwt";
+import { use } from "hono/jsx";
+// import { env } from "hono/adapter";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
+//middleware
+app.use("/api/v1/blog/*", async (c, next) => {
+  const header = c.req.header("authorization")?.split(" ")[1] || "";
+
+  const response = await verify(header, c.env.JWT_SECRET);
+  if (response.id()) {
+    next();
+  } else {
+    c.status(403);
+    return c.json({ error: "unauthorized" });
+  }
+});
 //user signup
-app.post("/api/v1/user/signup", (c) => {
-  return c.text("Hello Hono!");
+app.post("/api/v1/user/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const { email, password, name } = await c.req.json();
+  const user = await prisma.user.create({ data: { email, password, name } });
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt });
 });
 //user signin
-app.post("/api/v1/user/signin", (c) => {
-  return c.text("Hello Hono!");
+app.post("/api/v1/user/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const { email } = await c.req.json();
+  const user = await prisma.user.findUniqueOrThrow({ where: { email } });
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "user not found" });
+  }
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt });
 });
 //create blog
 app.post("/api/v1/blog", (c) => {
